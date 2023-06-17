ddbf1d91d9227677f8990e7eabdb1c55b0878558
H1rono <hronok66@gmail.com>
2023-06-17 16:49:44 +0900

    コメント追加

--- stam_prolog/evaluate/match.py
+++ stam_prolog/evaluate/match.py
@@ -2,5 +2,13 @@ from ..ast import Stamps, VarStamps
 
 
 def match_stamps(stamps: Stamps, var_stamps: VarStamps) -> bool:
-    """ """
+    """
+    TODO
+    スタンプがマッチするかどうかを判定する
+    ex:
+    stamps = [Stamp("technologist"), Stamp("heart"), Stamp("girl"), Stamp("computer")]
+    var_stamps = [Stamp("technologist"), Variable("x"), Variable("computer")]
+    ---
+    variableは1つ以上のスタンプにマッチする
+    """
     raise NotImplementedError
