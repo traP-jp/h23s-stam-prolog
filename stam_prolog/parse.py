9067ad9843bfaefd00c5676e24b0fa497f3ea290
Kamaboko <kamaboko002700@gmail.com>
2023-06-17 12:57:06 +0900

    todoの内容だけ戻した

--- stam_prolog/parse.py
+++ stam_prolog/parse.py
@@ -12,6 +12,14 @@ def extract_stamps(src: str) -> list[str]:
 
 
 def split_sentences(src: list[str]) -> Union[list[list[str]], str]:
+    """
+    スタンプ列を文ごとに分割する
+    ex. ":technologist: :heart: :computer: :ton: :heart: :computer: :ton:".split()
+    -> [
+        ":technologist: :heart: :computer: :ton:".split(),
+        ":heart: :computer: :ton:".split()
+    ]
+    """
     l: list[list[str]] = [[]]
     i = 0
     for s in src:
