b892876500858d52d3b1379fb6b17948e1ddcc1b
Kamaboko <kamaboko002700@gmail.com>
2023-06-17 17:14:55 +0900

    match原案(1変数)

--- stam_prolog/evaluate/match.py
+++ stam_prolog/evaluate/match.py
@@ -1,14 +1,33 @@
-from ..ast import Stamps, VarStamps
+from ..ast import Stamps, VarStamps, Variable
 
 
 def match_stamps(stamps: Stamps, var_stamps: VarStamps) -> bool:
     """
-    TODO
     スタンプがマッチするかどうかを判定する
     ex:
     stamps = [Stamp("technologist"), Stamp("heart"), Stamp("girl"), Stamp("computer")]
-    var_stamps = [Stamp("technologist"), Variable("x"), Variable("computer")]
+    var_stamps = [Stamp("technologist"), Variable("x"), Stamp("computer")]
     ---
     variableは1つ以上のスタンプにマッチする
+    →xにStamp("heart"),Stamp("girl")を代入したらマッチする
     """
-    raise NotImplementedError
+    former = []
+    latter = []
+    verpass = False
+    if len(stamps) < len(var_stamps):
+        return False
+    for ver in var_stamps:
+        if not verpass:
+            if ver != Variable:
+                former.append(ver)
+            else:
+                verpass = True
+        else:
+            latter.append(ver)
+    for i in range(len(former)):
+        if former[i] != stamps[i]:
+            return False
+    for j in range(len(latter)):
+        if latter[j] != stamps[len(var_stamps) - len(latter) + j]:
+            return False
+    return True
