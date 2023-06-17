67fc8921ee315cec1871b3c0e5e612dd22a590fa
H1rono <hronok66@gmail.com>
2023-06-17 17:12:47 +0900

    __errored

--- stam_prolog/evaluate/evaluator.py
+++ stam_prolog/evaluate/evaluator.py
@@ -2,16 +2,21 @@ from ..ast import ConditionalStatement, DeclStatement, QueryStatement
 
 
 class Evaluator:
-    __slots__ = ("__declarations", "__cond_declarations")
+    __slots__ = ("__declarations", "__cond_declarations", "__errored")
 
     def __init__(self) -> None:
         self.__declarations: set[DeclStatement] = set()
         self.__cond_declarations: set[ConditionalStatement] = set()
+        self.__errored = False
 
     def eval_decl_statement(self, statement: DeclStatement) -> None:
+        if self.__errored:
+            return
         self.__declarations.add(statement)
 
     def eval_conditional_statement(self, statement: ConditionalStatement) -> None:
+        if self.__errored:
+            return
         self.__cond_declarations.add(statement)
 
     def eval_query_statement(self, statement: QueryStatement) -> None:
