b96824b67a025d7c4278ed6765436a572d54a29c
Pina641 <kakinotane2048@gmail.com>
2023-06-17 12:07:57 +0900

    extract_stamps の更新

--- stam_prolog/parse.py
+++ stam_prolog/parse.py
@@ -8,6 +8,28 @@ def extract_stamps(src: str) -> List[str]:
     ex. "@BOT_stamProlog :technologist: :heart::computer:\n:ton:"
     -> ":technologist: :heart: :computer: :ton:".split()
     """
+    src_size = len(src)
+    is_in_stamp = False
+    list_stamp = []
+    now_stamp = ""
+    
+    for i in range(src_size):
+        if src[i] == ":":
+            if is_in_stamp:
+                is_in_stamp = False
+                list_stamp.append(now_stamp)
+                now_stamp = ""
+            else:
+                is_in_stamp = True
+        elif not is_in_stamp:
+            continue
+        elif ("0" <= src[i] and src[i] <= "9") or ("a" <= src[i] and src[i] <= "z") or ("A" <= src[i] and src[i] <= "Z") or src[i] == "_" or src[i] == "-" or src[i] == ".":
+            now_stamp += src[i]
+        else:
+            is_in_stamp = False
+            now_stamp = ""
+    
+    return list_stamp
     raise NotImplementedError
 
 
