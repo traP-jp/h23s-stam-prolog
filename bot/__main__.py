a528e89746464c56e8c59f8460a0fb69520f1fed
H1rono <hronok66@gmail.com>
2023-06-17 12:00:54 +0900

    mainスクリプト
    `poetry run python3 -m bot

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,1 +1,15 @@
-# TODO
+from traq_bot import TraqBot
+
+from . import Env, Handler
+
+
+def main() -> None:
+    env = Env.from_env()
+    bot = TraqBot(env.verification_token)
+    handler = Handler(env.bot_access_token)
+    bot.message_created(handler.on_message_created)
+    bot.run(8080)
+
+
+if __name__ == "__main__":
+    main()
