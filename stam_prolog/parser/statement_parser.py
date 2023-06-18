a98d062cd4ac64abf2364a788ba1f82d7a79d81b
H1rono <hronok66@gmail.com>
2023-06-18 13:50:27 +0900

    statement_parser書いた

--- stam_prolog/parser/statement_parser.py
+++ stam_prolog/parser/statement_parser.py
@@ -1,6 +1,9 @@
-from typing import Optional
+from typing import Optional, Union
+
+from frozenlist import FrozenList
 
 from .. import ast
+from .math.math_parser import parse_math
 from .stamp_kind import StampKind
 from .statement_kind import StatementKind
 
@@ -49,11 +52,75 @@ class StatementParser:
             res.append(StampKind.Normal)
         return res
 
+    def parse_single_statement(
+        self, stamps: list[str]
+    ) -> Union[ast.SingleStatement, ast.VarSingleStatement]:
+        res = []
+        kinds = self.judge_stamp_kinds(stamps)
+        math_stack: list[tuple[StampKind, str]] = []
+        ss_stack: list[ast.Atom | ast.Variable] = []
+        for k, s in zip(kinds, stamps):
+            if k & (StampKind.MathNumber | StampKind.MathSign | StampKind.MathOperator):
+                math_stack.append((k, s))
+                continue
+            if math_stack:
+                ss_stack.append(parse_math(math_stack))
+                math_stack.clear()
+            if k & StampKind.Variable:
+                ss_stack.append(ast.Variable(s))
+                continue
+            if k & StampKind.And:
+                fl = FrozenList(ss_stack)
+                fl.freeze()
+                res.append(fl)
+                ss_stack.clear()
+                continue
+            ss_stack.append(ast.Stamp(s))
+        fl = FrozenList(res)  # type: ignore
+        fl.freeze()
+        return FrozenList([fl])
+
+    def parse_cond_statement(self, stamps: list[str]) -> ast.ConditionalStatement:
+        res_l = []
+        res_r = []
+        kinds = self.judge_stamp_kinds(stamps)
+        math_stack: list[tuple[StampKind, str]] = []
+        ss_stack: list[ast.Atom | ast.Variable] = []
+        read_arrow = False
+        for k, s in zip(kinds, stamps):
+            if k & (StampKind.MathNumber | StampKind.MathSign | StampKind.MathOperator):
+                math_stack.append((k, s))
+                continue
+            if math_stack:
+                ss_stack.append(parse_math(math_stack))
+                math_stack.clear()
+            if k & StampKind.Variable:
+                ss_stack.append(ast.Variable(s))
+                continue
+            if k & StampKind.And:
+                fl = FrozenList(ss_stack)
+                fl.freeze()
+                if read_arrow:
+                    res_r.append(fl)
+                else:
+                    res_l.append(fl)
+                ss_stack.clear()
+                continue
+            ss_stack.append(ast.Stamp(s))
+        fl_l = FrozenList(res_l)
+        fl_r = FrozenList(res_r)
+        return ast.ConditionalStatement.new(fl_l, fl_r)
+
     def parse(
         self, stamps: list[str]
     ) -> Optional[ast.DeclStatement | ast.QueryStatement]:
-        self.s_kind = StatementKind.judge(stamps)
-        if self.s_kind == StatementKind.NormalDeclaration:
-            self.next_stamp_kind ^= StampKind.Variable
-        # kinds = self.judge_stamp_kinds(stamps[:-1])
+        kind = StatementKind.judge(stamps)
+        if kind == StatementKind.NormalDeclaration:
+            return self.parse_single_statement(stamps)
+        if kind == StatementKind.ConditionalDeclaration:
+            return self.parse_cond_statement(stamps)
+        if kind == StatementKind.NormalQuery:
+            return self.parse_single_statement(stamps)
+        if kind == StatementKind.ConditionalQuery:
+            return self.parse_cond_statement(stamps)
         raise NotImplementedError
