88dafefb063be0e13ce8406aedafb0012acce39c
H1rono <hronok66@gmail.com>
2023-06-18 13:23:21 +0900

    parse_math

--- stam_prolog/parser/math/math_parser.py
+++ stam_prolog/parser/math/math_parser.py
@@ -1,3 +1,4 @@
+from ast import Expression
 from typing import Union
 
 from ...ast import math
@@ -16,3 +17,43 @@ def split_tokens(src: list[tuple[StampKind, str]]) -> list[Union[math.Literal, s
             continue
         literal_stack.append(s)
     return res
+
+
+def parse_math(src: list[tuple[StampKind, str]]) -> Expression:
+    tokens = split_tokens(src)
+    output: list[math.Expression] = []
+    operator_stack: list[str] = []
+
+    def push_operator(op: str) -> None:
+        if op == ":heavy_plus_sign:":
+            output.append(math.AddExpression(output.pop(), output.pop()))
+        elif op == ":heavy_minus_sign:":
+            output.append(math.SubExpression(output.pop(), output.pop()))
+        elif op == ":heavy_multiplication_x:":
+            output.append(math.MulTerm(output.pop(), output.pop()))
+        elif op == ":heavy_division_sign:":
+            output.append(math.DivTerm(output.pop(), output.pop()))
+        else:
+            raise NotImplementedError
+
+    for token in tokens:
+        if isinstance(token, math.Literal):
+            output.append(token)
+            continue
+        if not operator_stack:
+            operator_stack.append(token)
+            continue
+        if token in [":heavy_plus_sign:", ":heavy_minus_sign:"]:
+            while operator_stack:
+                push_operator(operator_stack.pop())
+            operator_stack.append(token)
+            continue
+        if token in [":heavy_multiplication_x:", ":heavy_division_sign:"]:
+            while operator_stack and operator_stack[-1] in [
+                ":heavy_multiplication_x:",
+                ":heavy_division_sign:",
+            ]:
+                push_operator(operator_stack.pop())
+            operator_stack.append(token)
+            continue
+    raise NotImplementedError
