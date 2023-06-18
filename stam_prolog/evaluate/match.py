9b720e1f30aaa068ebf36c97bafb8ace049b346e
kenken714 <kenkenkeiko043@gmail.com>
2023-06-18 12:47:18 +0900

    Update match.py

--- stam_prolog/evaluate/match.py
+++ stam_prolog/evaluate/match.py
@@ -1,8 +1,12 @@
-from typing import Optional
+from typing import Optional, Union
 
 from frozenlist import FrozenList
 
-from ..ast import Stamps, Variable, VarSingleStatement, VarStamps
+from ..ast import Atom, Stamps, Variable, VarSingleStatement, VarStamps
+
+
+def is_var(stamp: Union[Atom, Variable]) -> bool:
+    return type(stamp) is Variable
 
 
 def match_stamps(
@@ -18,7 +22,58 @@ def match_stamps(
     返り値はどのVariableがどのようなスタンプ列に置き換えられるかを示す
     そもそもマッチしなかったらNoneを返す
     """
-    raise NotImplementedError
+    length_stamps = len(stamps)
+    length_var_stamps = len(var_stamps)
+    is_in_var = False
+    var_begin = 0
+    var = []
+    var_cnt = sum(1 for stamp in var_stamps if is_var(stamp))
+    var_seen = 0
+    i = 0
+    j = 0
+
+    while i < length_stamps:
+        if is_var(var_stamps[j]):
+            var_seen += 1
+            if var_seen == var_cnt:
+                var.append([j, i, length_stamps - length_var_stamps + j])
+                i = length_stamps - (length_var_stamps - j - 1)
+                j += 1
+            elif j == length_var_stamps - 1:
+                var.append([j, i, length_stamps - 1])
+                j += 1
+            else:
+                if is_var(var_stamps[j + 1]):
+                    var.append([j, i, i])
+                    j += 1
+                else:
+                    var_begin = i
+                    is_in_var = True
+                    j += 1
+        else:
+            if is_in_var:
+                if stamps[i] == var_stamps[j]:
+                    var.append([j - 1, var_begin, i - 1])
+                    j += 1
+                    is_in_var = False
+                else:
+                    pass  # i += 1
+            else:
+                if stamps[i] == var_stamps[j]:
+                    j += 1
+                else:
+                    break
+        if j == length_var_stamps:
+            break
+        i += 1
+    matched_dict: dict[Variable, Stamps] = {}
+    for x in var:
+        for i in range(x[1], x[2] + 1):
+            matched_dict[var_stamps[x[0]]].append(Stamps)
+    if len(matched_dict):
+        return matched_dict
+    else:
+        return None
 
 
 def papply_match(match: dict[Variable, Stamps], var_stamps: VarStamps) -> VarStamps:
