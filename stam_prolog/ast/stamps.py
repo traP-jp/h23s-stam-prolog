b78b2f4ce623f481d29b4bb157cfe7d57e226c95
H1rono <hronok66@gmail.com>
2023-06-18 11:26:50 +0900

    stamps.to_str

--- stam_prolog/ast/stamps.py
+++ stam_prolog/ast/stamps.py
@@ -19,3 +19,21 @@ class Variable:
 Atom = Union[Stamp, Expression]
 Stamps = FrozenList[Atom]
 VarStamps = FrozenList[Union[Atom, Variable]]
+
+
+def _num2str(n: int) -> str:
+    if n < 0:
+        return f":heavy_minus_sign:{_num2str(-n)}"
+    if n < 10:
+        s = "zero one two three four five six seven eight nine".split()[n]
+        return f":{s}:"
+    return f"{_num2str(n // 10)}:{_num2str(n % 10)}"
+
+
+def to_str(statement: Union[Atom, Variable]) -> str:
+    if isinstance(statement, Variable):
+        return f":{statement.name}:"
+    elif isinstance(statement, Stamp):
+        return f":{statement.name}:"
+    else:
+        return _num2str(statement.eval())
