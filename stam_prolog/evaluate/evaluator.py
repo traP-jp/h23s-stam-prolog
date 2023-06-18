33a2f61f533afb9a0045f3299f441eaa091e20b8
H1rono <hronok66@gmail.com>
2023-06-18 09:34:14 +0900

    eval_decl_statement追加

--- stam_prolog/evaluate/evaluator.py
+++ stam_prolog/evaluate/evaluator.py
@@ -1,3 +1,5 @@
+from stam_prolog.ast.statement import DeclStatement
+
 from ..ast import ConditionalStatement, QueryStatement, SingleStatement, Stamps
 from .match import apply_match, contextful_match
 
@@ -68,6 +70,14 @@ class Evaluator:
         if self.__errored:
             return
         self.__cond_declarations.add(statement)
+
+    def eval_decl_statement(self, statement: DeclStatement) -> None:
+        if self.__errored:
+            return
+        if isinstance(statement, ConditionalStatement):
+            self.eval_conditional_statement(statement)
+        else:
+            self.eval_single_statement(statement)
 
     def eval_query_statement(self, statement: QueryStatement) -> None:
         ...
