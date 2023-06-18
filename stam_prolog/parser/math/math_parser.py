d5622f955a7dd4d3d8b53603ef9e38bbcb97e2b7
H1rono <hronok66@gmail.com>
2023-06-18 13:00:28 +0900

    math.split_tokens

--- stam_prolog/parser/math/math_parser.py
+++ stam_prolog/parser/math/math_parser.py
@@ -1,1 +1,18 @@
-# from ...ast import math
+from typing import Union
+
+from ...ast import math
+from ..stamp_kind import StampKind
+
+
+def split_tokens(src: list[tuple[StampKind, str]]) -> list[Union[math.Literal, str]]:
+    res: list[Union[math.Literal, str]] = []
+    literal_stack: list[str] = []
+    for k, s in src:
+        if k & StampKind.MathOperator:
+            literal = math.Literal.from_stamps(literal_stack)
+            literal_stack.clear()
+            res.append(literal)
+            res.append(s)
+            continue
+        literal_stack.append(s)
+    return res
