2740bd3a4185b9d1fff9add28f017a77f7d7b6fc
H1rono <hronok66@gmail.com>
2023-06-18 11:42:30 +0900

    Parserクラス雛形

--- stam_prolog/parser/parser.py
+++ stam_prolog/parser/parser.py
@@ -1,2 +1,9 @@
+from typing import Union
+
+from .. import ast
+
+
 class Parser:
-    ...
+    def parse(self, src: str) -> list[Union[ast.DeclStatement, ast.QueryStatement]]:
+        # TODO
+        raise NotImplementedError
