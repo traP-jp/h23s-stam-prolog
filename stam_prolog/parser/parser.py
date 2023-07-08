7472787ed39edfcb03e57e4d534c89773c559df2
H1rono <hronok66@gmail.com>
2023-07-09 08:21:49 +0900

    :recycle: s/sentence/statement/g

--- stam_prolog/parser/parser.py
+++ stam_prolog/parser/parser.py
@@ -1,7 +1,7 @@
 from typing import Union
 
 from .. import ast
-from ..parse import extract_stamps, split_sentences
+from ..parse import extract_stamps, split_statements
 from .statement_parser import StatementParser
 
 
@@ -10,7 +10,7 @@ class Parser:
         self, src: str
     ) -> Union[list[tuple[bool, Union[ast.DeclStatement, ast.QueryStatement]]], str]:
         stamps = extract_stamps(src)
-        statements = split_sentences(stamps)
+        statements = split_statements(stamps)
         if isinstance(statements, str):
             return statements
         res = []
